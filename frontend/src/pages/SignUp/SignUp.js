831d9456d9100c4e016e2328d292932e5fd001df
Jessica Young <jessica.young@berkeley.edu>
2022-10-16 04:00:37 -0700

    signup progress

--- frontend/src/pages/SignUp/SignUp.js
+++ frontend/src/pages/SignUp/SignUp.js
@@ -1,5 +1,7 @@
 import React, {useState} from "react";
 import "./SignUp.css"
+import TextField from "@material-ui/core/TextField";
+import Button from '@mui/material/Button';
 import { useAtom } from "jotai";
 import { currUserAtom } from "../../atoms";
 
@@ -59,45 +61,110 @@ function SignUp() {
 
 
     return (
-        <div className="signup">
-            <p className="text-large">wake the f*ck up</p>
-            <br />
-            <p className="text-subtitle">never sleep through an alarm again</p>
+      <div class="signup">
+        <div class="left-half">
+          <div class="signup-stuff">
+            <div class="text-large">
+              create an account
+            </div>
+
+            <div class="text-subtitle">
+              never sleep through an alarm again.
+            </div>
+
             <div>
-                <p className="form-title">username</p>
-                <br />
-                <input className='inputSignup' type="text" {...username} />
+              <div class="form-title">username</div>
+
+              <TextField
+                style={{ width: "100%" }}
+                variant="outlined"
+                onChange={handleUsernameChange}
+              />
             </div>
+
             <div style={{ marginTop: 10 }}>
-                <p className="form-title">password</p>
-                <br />
-                <input className='inputSignup' type="password" {...password} />
+              <div class="form-title">password</div>
+
+              <TextField
+                style={{ width: "100%" }}
+                variant="outlined"
+                onChange={handlePasswordChange}
+              />
             </div>
+
             <div style={{ marginTop: 10 }}>
-                <p className="form-title">street name</p>
-                <br />
-                <input className='inputSignup' type="streetName" {...streetName} />
+              <div class="form-title">phone number</div>
+
+              <TextField
+                style={{ width: "100%" }}
+                variant="outlined"
+                onChange={handlePasswordChange}
+              />
             </div>
-            <div style={{ marginTop: 10 }}>
-                <p className="form-title">city</p>
-                <br />
-                <input className='inputSignup' type="city" {...city} />
-            </div>
-            <div style={{ marginTop: 10 }}>
-                <p className="form-title">state</p>
-                <br />
-                <input className='inputSignup' type="state" {...state} />
-            </div>
-            <div style={{ marginTop: 10 }}>
-                <p className="form-title">phone</p>
-                <br />
-                <input className='inputSignup' type="phone" {...phone} />
-            </div>
-            {error && <><small style={{ color: 'red' }}>{error}</small><br /></>}<br />
-            <a href = "http://localhost:3000" className=" dont fucking know">Have an account? Login here.</a>
+
             <br />
-            <input className='btnLogin' type="button" value={loading ? 'Loading...' : 'Sign Up'} onClick={handleSignUp} disabled={loading} /><br />
+
+            <Button 
+              style={{
+                backgroundColor: "rgba(186,209,250)", fontFamily: "DM SANS", textTransform: "lowercase", color: "black", boxShadow: "none"
+            }}
+            variant="contained"
+            // disabled={username === "", password === ""}
+          >
+            sign up
+          </Button>
+
+          <a href = "http://localhost:3000" class="form-extra">
+            already have an account? login here.
+          </a>
+
+          </div>
         </div>
+        <div class="right-half">
+          <div class="rectangle">
+            <img class="tire" src={require("../../assets/blankibee.png")}></img>
+          </div>
+        </div>
+      </div>
+        // <div className="signup">
+        //     <p className="text-large">wake the f*ck up</p>
+        //     <br />
+        //     <p className="text-subtitle">never sleep through an alarm again</p>
+        //     <div>
+        //         <p className="form-title">username</p>
+        //         <br />
+        //         <input className='inputSignup' type="text" {...username} />
+        //     </div>
+        //     <div style={{ marginTop: 10 }}>
+        //         <p className="form-title">password</p>
+        //         <br />
+        //         <input className='inputSignup' type="password" {...password} />
+        //     </div>
+        //     <div style={{ marginTop: 10 }}>
+        //         <p className="form-title">street name</p>
+        //         <br />
+        //         <input className='inputSignup' type="streetName" {...streetName} />
+        //     </div>
+        //     <div style={{ marginTop: 10 }}>
+        //         <p className="form-title">city</p>
+        //         <br />
+        //         <input className='inputSignup' type="city" {...city} />
+        //     </div>
+        //     <div style={{ marginTop: 10 }}>
+        //         <p className="form-title">state</p>
+        //         <br />
+        //         <input className='inputSignup' type="state" {...state} />
+        //     </div>
+        //     <div style={{ marginTop: 10 }}>
+        //         <p className="form-title">phone</p>
+        //         <br />
+        //         <input className='inputSignup' type="phone" {...phone} />
+        //     </div>
+        //     {error && <><small style={{ color: 'red' }}>{error}</small><br /></>}<br />
+        //     <a href = "http://localhost:3000" className=" dont fucking know">Have an account? Login here.</a>
+        //     <br />
+        //     <input className='btnLogin' type="button" value={loading ? 'Loading...' : 'Sign Up'} onClick={handleSignUp} disabled={loading} /><br />
+        // </div>
     )
 }
 
