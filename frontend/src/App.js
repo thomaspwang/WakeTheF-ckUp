bf9b42009a771f9a4f6c55b1edea38dc2372116a
Thomas Wang <thomaswang@berkeley.edu>
2022-10-16 00:35:26 -0700

    kill me

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import Login from './pages/Login/Login';
 import Alarm from './pages/Alarm/Alarm';
 import SignUp from './pages/SignUp/SignUp';
 import FriendSearch from './components/FriendSearch/FriendSearch'
+import FriendsDropdown from './components/FriendsDropdown/FriendsDropdown';
+import Friends from './pages/Friends/Friends';
 
 function App() {
 
