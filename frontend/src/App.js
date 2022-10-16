48e8b947820b22b10859a33ffbf66f39e8f97f10
Thomas Wang <thomaswang@berkeley.edu>
2022-10-15 21:08:41 -0700

    Added timer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,13 @@ import './App.css';
 import {useAtom} from 'jotai'
 import {currUserAtom} from "./atoms.js"
 import Login from './pages/Login/Login';
-import Home from './pages/Home/Home';
+import Alarm from './pages/Alarm/Alarm';
 
 function App() {
 
   const [user] = useAtom(currUserAtom);
 
-  if (user == '') {
-    return <Login/>
-  } else {
-    return <Home/>
-  }
-  
+  return <Alarm/>
 }
 
 export default App;
