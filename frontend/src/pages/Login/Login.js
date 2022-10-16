26497808d9f8b9e85fe781afa7b6556168cd5f9a
Jessica Young <jessica.young@berkeley.edu>
2022-10-16 03:17:55 -0700

    login page finally

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,3 +1,6 @@
+import { requirePropFactory } from "@mui/material";
+import TextField from "@material-ui/core/TextField";
+import Button from '@mui/material/Button';
 import React, {useState} from "react";
 import "./Login.css"
 import { useNavigate } from "react-router-dom";
@@ -8,6 +11,7 @@ const useFormInput = initialValue => {
   const handleChange = e => {
     setValue(e.target.value);
   }
+
   return {
     value,
     onChange: handleChange
@@ -15,10 +19,21 @@ const useFormInput = initialValue => {
 }
 
 function Login() {
-  const username = useFormInput('');
-  const password = useFormInput('');
+  // const username = useFormInput('');
+  // const password = useFormInput('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+
+  const handlePasswordChange = e => {
+    setPassword(e.target.password);
+  }
+
+  const handleUsernameChange = e => {
+    setUsername(e.target.username);
+  }
+
   return (
     <div class="login">
       <div class="left-half">
@@ -28,26 +43,48 @@ function Login() {
           </div>
             
           <div class="text-subtitle">
-            never sleep through an alarm again.
+            using the power of friendship.
           </div>
 
           <div>
             <div class="form-title">username</div>
-            {/* <br /> */}
-            <input class='inputLogin' type="text" {...username} autoComplete="new-password" />
+
+            <TextField
+              style={{ width: "100%" }}
+              variant="outlined"
+              onChange={handleUsernameChange}
+            />
+
+            {/* <input class='inputLogin' type="text" {...username} autoComplete="new-password" /> */}
           </div>
 
           <div style={{ marginTop: 10 }}>
             <div class="form-title">password</div>
-            {/* <br /> */}
-            <input class='inputLogin' type="password" {...password} autoComplete="new-password" />
+
+            <TextField
+              style={{ width: "100%" }}
+              variant="outlined"
+              onChange={handlePasswordChange}
+            />
+
+            {/* <input class='inputLogin' type="password" {...password} autoComplete="new-password" /> */}
           </div>
 
           <br />
 
           {error && <><small style={{ color: 'red' }}>{error}</small><br /></>}
 
-          <input class='btnLogin' type="button" value={loading ? 'Loading...' : 'Login'} disabled={loading} />
+          <Button 
+              style={{
+                backgroundColor: "rgba(186,209,250)", fontFamily: "DM SANS", textTransform: "lowercase", color: "black", boxShadow: "none"
+            }}
+            variant="contained"
+            disabled={username === "", password === ""}
+          >
+            login
+          </Button>
+
+          {/* <input class='btnLogin' type="button" value={loading ? 'Loading...' : 'Login'} disabled={loading} /> */}
 
           <div class="form-extra">
             don't have an account? sign up here.
@@ -56,7 +93,7 @@ function Login() {
       </div>
       <div class="right-half">
         <div class="rectangle">
-          
+          <img class="tire" src={require("../../assets/tirealarmbee.png")}></img>
         </div>
       </div>
     </div>
