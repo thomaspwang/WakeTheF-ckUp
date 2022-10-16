6544f7d58d91a3661eb9d0f11e34a2577b445284
Thomas Wang <thomaswang@berkeley.edu>
2022-10-15 22:48:51 -0700

    signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,13 @@ import {useAtom} from 'jotai'
 import {currUserAtom} from "./atoms.js"
 import Login from './pages/Login/Login';
 import Alarm from './pages/Alarm/Alarm';
+import SignUp from './pages/SignUp/SignUp';
 
 function App() {
 
   const [user] = useAtom(currUserAtom);
 
-  return <Login/>
+  return <SignUp/>
 }
 
 export default App;
