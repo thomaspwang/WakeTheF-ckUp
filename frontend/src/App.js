c035872f179f171ce1ad2adf3e4f6ccae8282b5f
Thomas Wang <thomaswang@berkeley.edu>
2023-10-16 14:41:51 -0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import { BrowserRouter as Router, Routes, Route }
     from 'react-router-dom';
 import './App.css';
-import {useAtom} from 'jotai'
-import {currUserAtom} from "./atoms.js"
-import Login from './pages/Login/Login';
-import Alarm from './pages/Alarm/Alarm';
-import SignUp from './pages/SignUp/SignUp';
-import FriendSearch from './components/FriendSearch/FriendSearch'
-import FriendsDropdown from './components/FriendsDropdown/FriendsDropdown';
-import Friends from './pages/Friends/Friends';
 
 function App() {
   
